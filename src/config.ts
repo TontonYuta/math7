87b6fa05dfc6bd98e0f17cf1e3b94cc321e1976d
TontonYuta <huyhoang6altt@gmail.com>
2026-03-14 19:09:47 +0700

    update

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-export const APP_VERSION = '2.2.2';
+export const APP_VERSION = '1.0.1';
 
 // Link Web App của Google Apps Script (Bạn sẽ thay link của bạn vào đây)
-export const GOOGLE_SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbwtoV7WcEcmhCelfp1UFnA8tb23OgdbF3tx--MVhK7CHQx1MRXmkUUfqpRMU3-ASp4bYA/exec';
+export const GOOGLE_SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbxBc5I2hOp5HQ8N3FfIMHyxjuQDd9-w5S-Bx86X867TeTScp4cDJBPRnvyNHgEijqKMQA/exec';
 
 // Sử dụng chung 1 link cho cả việc lưu điểm (POST) và kiểm tra phiên bản (GET)
 export const VERSION_CHECK_URL = GOOGLE_SHEET_API_URL;
